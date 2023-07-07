e966b65b561dd11f1f62ea9431da9b154607a020
Ever Solis <eversolis@theblackbit.io>
2023-07-06 18:31:12 -0600

    Portfolio section created

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        fadeIn: "fadeIn 0.5s ease-in forwards",
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
+      },
+    },
   },
   plugins: [
     function ({ addVariant }) {
@@ -10,4 +20,8 @@ module.exports = {
       addVariant("child-hover", "& > *:hover");
     },
   ],
+
+  variants: {
+    animation: ["motion-safe"],
+  },
 };
